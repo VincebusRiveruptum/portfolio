f922e99ca722feeddb273ac23635701468592812
VincebusRiveruptum <vincebus@Vincebus-PC.>
2025-08-02 14:12:11 -0400

    ADD: Page meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,6 +23,28 @@ export default defineNuxtConfig({
           href: "/portfolio/logo_white.png",
         },
       ],
+      meta: [
+        {
+          name: "description",
+          content: "Full Stack Web Developer portfolio by Vicente Riveros.",
+        },
+        {
+          name: "keywords",
+          content: "Vue, Laravel, Nuxt, Full Stack Developer, Portfolio",
+        },
+        { name: "robots", content: "index, follow" },
+        { property: "og:title", content: "Vincebus Riveruptum Portfolio" },
+        {
+          property: "og:description",
+          content:
+            "Welcome to Vincebus Riveruptum's portfolio page, a Full Stack Web Developer from Chile.",
+        },
+        { property: "og:type", content: "website" },
+        {
+          property: "og:url",
+          content: "https://your-username.github.io/portfolio",
+        },
+      ],
     },
   },
 });
