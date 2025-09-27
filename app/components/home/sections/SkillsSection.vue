22d0cb23cd1c9c6187236db5dd17c5b88a3c72c6
VincebusRiveruptum <vincebus@Vincebus-PC.>
2025-09-26 21:06:44 -0300

    ADD: New bg

--- app/components/home/sections/SkillsSection.vue
+++ app/components/home/sections/SkillsSection.vue
@@ -8,7 +8,7 @@ const { skills } = useSkills();
 
 <template>
   <div
-    class="flex flex-col gap-16 bg-background-secondary p-12 sm:py-[8%] sm:px-[15%]"
+    class="flex flex-col gap-16 p-12 sm:py-[8%] sm:px-[15%]"
   >
     <h1>{{ $t("skills") }}</h1>
 
